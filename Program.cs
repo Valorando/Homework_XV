f14faa9da6b3a3f11be6b1dcf1db08c9608a2566
Valorando <vadimsokil.dnipro@gmail.com>
2023-08-24 02:56:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/*Написать программу которая будет принимать у пользователя название БД и создать ее. 
+После чего запрашивать у пользователя Название таблицы и создать ее с полями которые нужны пользователю. 
+После чего дать возможность ему наполнить ее) */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_24_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
